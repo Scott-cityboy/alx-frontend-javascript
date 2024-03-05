af8f16e92b4287443927de91513aa6127e0d7325
Scott-cityboy <scottimbiaka@gmail.com>
2024-03-05 10:33:26 +0000

    0x00. ES6 Basics

--- 0x00-ES6_basic/7-main.js
+++
@@ -1,7 +0,0 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
-console.log(getBudgetObject(400, 700, 900));
-
-bob@dylan:~$
-bob@dylan:~$ npm run dev 7-main.js 
-{ income: 400, gdp: 700, capita: 900 }
